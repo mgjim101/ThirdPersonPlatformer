1c6df059b54b679b3954122afa681cb0607d9394
Mohamed Gamal <mohamed.g.sakr@gmail.com>
2025-03-02 23:38:06 -0800

    coin disappear when player gets near the coin and working on the score

---
+++ Assets/Script/CoinDetection.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+using TMPro;
+public class CoinDetection : MonoBehaviour
+{
+    public TextMeshProGUI text;
+    public int score = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+ void OnTriggerEnter(Collider other) {
+        
+        if (other.tag == "Coin") {
+
+            score++;
+            text.text = "Score: " + score;
+            Destroy(other.gameObject);
+
+        }
+    }
+}
