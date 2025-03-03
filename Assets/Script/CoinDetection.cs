e333d4c8c3761a2624a530a893710c2cb1013adb
Mohamed Gamal <mohamed.g.sakr@gmail.com>
2025-03-02 23:44:46 -0800

    coin score implementation done and added more coins around the map and tuned the movement and jump

--- Assets/Script/CoinDetection.cs
+++ Assets/Script/CoinDetection.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using TMPro;
 public class CoinDetection : MonoBehaviour
 {
-    public TextMeshProGUI text;
+    public TextMeshProUGUI text;
     public int score = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
